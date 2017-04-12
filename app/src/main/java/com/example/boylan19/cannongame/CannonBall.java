688d3d677bd2fc291b09fa473f523797f444f009
TristanBoylan <boylan19@up.edu>
2017-04-12 14:17:50 -0700

    O

--- app/src/main/java/com/example/boylan19/cannongame/CannonBall.java
+++ app/src/main/java/com/example/boylan19/cannongame/CannonBall.java
@@ -8,59 +8,61 @@ import android.view.MotionEvent;
 /**
  * Created by boylan19 on 4/4/2017.
  */
-public class CannonBall implements Animator {
-    private int count = 0;
-    private double para = 0.1;
-    private boolean goBackwards = true;
-    private double xCord;
-    private double yCord;
-    private int angle;
+public class CannonBall {
 
+    private double xPosition;
+    private double yPosition;
+    private double xVelocity;
+    private double yVelocity;
+    private static double acceleration;
+    private static double yWindspeed;
+    private static double xWindspeed;
 
-    public int interval() {
-        return 30;
-    }
-    public int backgroundColor() {
-        // create/return the background color
-        return Color.rgb(180, 200, 255);
-    }
-    public void tick(Canvas g) {
-        count++;
-        para = para *1.025;
+    public CannonBall(double initXposition, double initXvelocity, double initYposition, double initYvelocity) {
+        Paint ball = new Paint();
+        ball.setColor(Color.BLACK);
+        ball.setStyle(Paint.Style.FILL);
 
-        xCord = ((count*15)*Math.cos(angle));
-        yCord = ((count * 15) - (para * ((count*15) ^ 2)));//TODO add funtion to calculate hight
-
-       // if (num < 0) num += 1200;
-        Paint redPaint = new Paint();
-        redPaint.setColor(Color.BLACK);
-        g.drawCircle((int)yCord, (int)xCord, 60, redPaint);
-
+        this.xPosition = initXposition;
+        this.xVelocity = initXvelocity;
+        this.yPosition = initYposition;
+        this.yVelocity = initYvelocity;
     }
 
-
-    public boolean doPause() {
-        if (goBackwards)
-        {
-            return true;
-        }
-        else {
-            return false;
-        }
+    public double GetXPos() {
+        return xPosition;
     }
-    public boolean doQuit() {
-        return false;
+    public double GetXVel() {
+        return xVelocity;
+    }
+    public double GetYPos() {
+        return yPosition;
+    }
+    public double GetYVel() {
+        return yVelocity;
     }
 
-    public void onTouch(MotionEvent event)
-    {
-        if (event.getAction() == MotionEvent.ACTION_DOWN)
-        {
-            goBackwards = !goBackwards;
-            count =3;
-            para = 0.1;
+    public void ReDrawCannonBall() {
+        xPosition += xVelocity + xWindspeed;
+        yVelocity += acceleration;
+        yPosition += yVelocity + yWindspeed;
+        if(yPosition<0) {
+            yPosition=0;
+            Ground();
         }
     }
+    public void Ground() {
+        yVelocity *= -0.2;
+        if(xVelocity<1 && xVelocity>=0) xVelocity=1;
+        else xVelocity=-1;
+    }
 
+    public void Shot() {
+        xVelocity *= -0.2;
+        yVelocity *= 0.3;
+        if(xVelocity<0) xPosition -= 20;
+        else xPosition += 20;
+    }
 
 }
+
